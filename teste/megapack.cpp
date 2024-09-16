5107d84a26709276a840f6af9df22275e2faadc4
joao <joaoemanuelsantos2019@gmail.com>
2024-09-16 13:16:47 -0300

    oi

--- teste/megapack.cpp
+++ teste/megapack.cpp
@@ -4,17 +4,29 @@ using namespace std;
 int main()
 {
 
-    int numero;
-    int soma = 0;
+    int n1;
+    int *n2 = 0;
 
-    cin >> numero;
-
-    for (int i = 1; i <= numero; ++i)
+    cin >> n1;
+    n2 = new int[n1];
+    for (int i = 0; i < n1; i++)
     {
-        numero += numero;
-        soma *= numero;
+        cin >> n2[i];
     }
-    cout << soma;
+    for (int i = 0; i < n1; i++)
+    {
+        if (n2[i] > 1, n2[i] < 0)
+        {
+            cout << n2[i] << ' ';
+        }
+    }
+    /*for (int i = 0; i < n1; i++)
+    {
+        if (n2[i] > 0)
+        {
+            cout << n2[i] << ' ';
+        }
+    }*/
 
     return 0;
 }
